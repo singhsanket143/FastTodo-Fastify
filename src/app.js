bb71f9d0f1818f9348eed78d7d37ca7bc5ef9df4
Sanket Singh <singhsanket143@gmail.com>
2024-05-28 21:53:40 +0530

    added codes

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const fp = require('fastify-plugin');
+const apiRouter = require('./routes/api/apiRouter');
+const db = require('./db/index');
+const todoRepository = require('./repository/todoRepository');
+const todoService = require('./services/todoService');
+async function app(fastify, options) {
+    await fastify.register(db);
+    await fastify.register(todoRepository);
+    await fastify.register(todoService);
+
+    await fastify.register(apiRouter, {prefix: '/api'});
+}
+
+module.exports = fp(app);
---
+++ src/routes/api/v1/v1Router.js
@@ -0,0 +1,6 @@
+const todoRouter = require('./todos/index');
+async function v1Router(fastify, options) {
+    fastify.register(todoRouter, {prefix: '/todos'});
+}
+
+module.exports = v1Router;
